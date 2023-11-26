ffb87cdf0bd45b32a706c2f5cec49b51539cef39
ReginYuan <2496943860@qq.com>
2023-11-26 22:45:07 +0800

    feat: 修改代码

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { resolve } from "path"; // 主要用于alias文件路径别名
 export default defineConfig({
   resolve: {
     alias: {
-      "@": resolve(__dirname, ".", "src")
+      "@": path.resolve(__dirname, "./src")
     }
   },
   server: {
