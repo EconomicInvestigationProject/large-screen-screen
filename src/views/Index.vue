83c7fc1d2eb5801115473af3468904648f7babd3
reginyuan <2496943860@qq.com>
2023-12-07 17:57:36 +0800

    feat: 修改服务器代码

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,29 +1,47 @@
 <template>
   <div class="flylineDiagram">
     <div class="flylineDiagram_left">
-      <chartpanel title="流动人口统计" class="flex-1 chart">
-        <v-chart :option="option1" style="height: 90%"></v-chart>
-      </chartpanel>
       <chartpanel title="各类人员占比" class="flex-1 chart">
-        <v-chart :option="option2" style="height: 90%"></v-chart>
-      </chartpanel>
-      <chartpanel title="流动人口统计" class="flex-1 chart">
-        <v-chart :option="option3" style="height: 90%"></v-chart>
-      </chartpanel>
-    </div>
-    <div class="flylineDiagram_content">
-      <Map></Map>
-    </div>
-    <div class="flylineDiagram_right">
-      <chartpanel title="流动人口统计" class="flex-1 chart">
         <v-chart :option="option1" style="height: 90%"></v-chart>
       </chartpanel>
-
-      <chartpanel title="各类人口占比" class="flex-1 chart">
+      <chartpanel title="流动人员占比" class="flex-1 chart">
         <v-chart :option="option2" style="height: 90%"></v-chart>
       </chartpanel>
       <div class="flex-1">
-        <chartpanel title="人员动态">
+        <chartpanel title="小区人员动态">
+          <vue3-seamless-scroll :list="newsdatas" class="indexscroll">
+            <div
+              class="item flex"
+              v-for="(item, index) in newsdatas"
+              :key="index"
+            >
+              <div class="flex_item">{{ item.title }}</div>
+              <div class="date">{{ item.date }}</div>
+              <div class="status">{{ item.status }}</div>
+            </div>
+          </vue3-seamless-scroll>
+        </chartpanel>
+      </div>
+    </div>
+    <div class="flylineDiagram_content">
+      <Map @sendMessageToParent="handleMessageFromChild"></Map>
+    </div>
+    <div class="flylineDiagram_right">
+      <chartpanel class="flex-1 chart" title="设备状态">
+        <div class="device">
+          <div class="device-item" v-for="item in device" :key="item.name">
+            <div class="device-item-name">{{ item.name }}</div>
+            <div class="device-item-ok">正常：{{ item.ok }}</div>
+            <div class="device-item-bad">损坏：{{ item.bad }}</div>
+            <div class="device-item-red">离线：{{ item.red }}</div>
+          </div>
+        </div>
+      </chartpanel>
+      <chartpanel title="流动人口统计" class="flex-1 chart">
+        <v-chart :option="option5" style="height: 90%"></v-chart>
+      </chartpanel>
+      <div class="flex-1">
+        <chartpanel title="搬离人员">
           <vue3-seamless-scroll :list="newsdatas" class="indexscroll">
             <div
               class="item flex"
@@ -47,32 +65,127 @@ import { faceList } from "../api/face";
 import Map from "../views/Map.vue";
 import { Vue3SeamlessScroll } from "vue3-seamless-scroll";
 import chartpanel from "@/components/chartpanel.vue";
-const config1 = reactive({
-  data: [
-    {
-      name: "南阳",
-      value: 167,
+import { ElMessage } from "element-plus";
+
+let option1 = reactive({
+  title: {
+    text: "人口占比比例",
+    textStyle: {
+      color: "#fff",
     },
-    {
-      name: "周口",
-      value: 67,
+    show: false,
+  },
+  tooltip: {
+    trigger: "item",
+  },
+  legend: {
+    top: "5%",
+    left: "center",
+    textStyle: {
+      color: "#fff", // 设置标题文字颜色为白色
     },
+  },
+  textStyle: {
+    color: "#fff",
+  },
+  series: [
     {
-      name: "漯河",
-      value: 123,
-    },
-    {
-      name: "郑州",
-      value: 55,
-    },
-    {
-      name: "西峡",
-      value: 98,
+      type: "pie",
+      radius: ["40%", "70%"],
+      avoidLabelOverlap: false,
+      itemStyle: {
+        borderRadius: 10,
+        borderColor: "#060E3F",
+        borderWidth: 2,
+      },
+      label: {
+        show: false,
+        position: "center",
+        textStyle: {
+          // 设置字体样式
+          color: "#fff", // 设置字体颜色为白色
+        },
+      },
+      emphasis: {
+        label: {
+          show: true,
+          fontSize: 40,
+          fontWeight: "bold",
+        },
+      },
+      labelLine: {
+        show: false,
+      },
+      data: [
+        { value: 12000, name: "常住人口" },
+        { value: 800, name: "流出人口" },
+        { value: 900, name: "重点人员" },
+        { value: 8000, name: "流入人口" },
+      ],
     },
   ],
 });
 
-const option1 = ref({
+let option2 = reactive({
+  title: {
+    text: "各类人口占比",
+    textStyle: {
+      color: "#fff",
+    },
+    show: false,
+  },
+  series: [
+    {
+      type: "pie",
+      label: {
+        color: "#fff",
+        fontStyle: "normal",
+        fontWeight: "bolder",
+      },
+      data: [
+        { name: "流入人口", value: 2000 },
+        { name: "流出人口", value: 3000 },
+      ],
+      insideLabel: {
+        show: true,
+      },
+    },
+  ],
+});
+
+let option3 = reactive({
+  title: {
+    text: "常住人口统计",
+    textStyle: {
+      color: "#fff",
+    },
+    show: false,
+  },
+  xAxis: {
+    type: "category",
+    boundaryGap: false,
+    data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+  },
+  yAxis: {
+    type: "value",
+  },
+  series: [
+    {
+      data: [820, 932, 901, 934, 1290, 1330, 1320],
+      type: "line",
+      areaStyle: {},
+    },
+  ],
+});
+
+// 设备状态
+let device = ref([
+  { name: "摄像机", ok: "1000", bad: "0", red: "0" },
+  { name: "传感器", ok: "2000", bad: "0", red: "0" },
+  { name: "路由器", ok: "123", bad: "0", red: "0" },
+]);
+
+let option5 = reactive({
   title: {
     text: "流动人口统计",
     textStyle: {
@@ -98,81 +211,124 @@ const option1 = ref({
   ],
 });
 
-const option2 = {
-  title: {
-    text: "各类人口占比",
-    textStyle: {
-      color: "#fff",
-    },
-    show: false,
-  },
-  series: [
-    {
-      type: "pie",
-      label: {
-        color: "#fff",
-        fontStyle: "normal",
-        fontWeight: "bolder",
-      },
-      data: [
-        { name: "常住人口", value: 93 },
-        { name: "重点人口", value: 32 },
-        { name: "暂住人口", value: 65 },
-        { name: "异常人口", value: 44 },
-      ],
-      insideLabel: {
-        show: true,
-      },
-    },
-  ],
+//社区动态数据
+const newsdatas = ref([
+  { title: "我王佳乐", date: "2023-01-01", status: "进小区" },
+  { title: "我王佳乐", date: "2023-01-01", status: "进小区" },
+  { title: "我王佳乐", date: "2023-01-01", status: "进小区" },
+  { title: "我王佳乐", date: "2023-01-01", status: "进小区" },
+  { title: "我王佳乐", date: "2023-01-01", status: "进小区" },
+  { title: "我王佳乐", date: "2023-01-01", status: "进小区" },
+  { title: "我王佳乐", date: "2023-01-01", status: "进小区" },
+  { title: "我王佳乐", date: "2023-01-01", status: "进小区" },
+  { title: "我王佳乐", date: "2023-01-01", status: "进小区" },
+  { title: "我王佳乐", date: "2023-01-01", status: "进小区" },
+  { title: "我王佳乐", date: "2023-01-01", status: "进小区" },
+  { title: "我王佳乐", date: "2023-01-01", status: "进小区" },
+]);
+
+const getList = async () => {};
+
+// 数据初始化全国数据
+const initialization = () => {
+  // 如果消息不是 "历下区"，则恢复 option2 到默认值
+
+  option1.series[0].data = [
+    { value: 12000, name: "常住人口" },
+    { value: 800, name: "流出人口" },
+    { value: 900, name: "重点人员" },
+    { value: 8000, name: "流入人口" },
+  ];
+
+  // 各类人口占比
+  option2.series[0].data = [
+    { name: "流入人口", value: 2000 },
+    { name: "流出人口", value: 3000 },
+  ];
+
+  // 常住人口统计初始化
+  option3.series[0].data = [820, 932, 901, 934, 1290, 1330, 1320];
+
+  // 流动人口统计初始化
+  option5.series[0].data = [120, 200, 150, 80, 70, 110, 130];
+
+  //社区动态数据初始化
+  newsdatas.value = [
+    { title: "我王佳乐", date: "2023-01-01", status: "进小区" },
+    { title: "我王佳乐", date: "2023-01-01", status: "进小区" },
+    { title: "我王佳乐", date: "2023-01-01", status: "进小区" },
+    { title: "我王佳乐", date: "2023-01-01", status: "进小区" },
+    { title: "我王佳乐", date: "2023-01-01", status: "进小区" },
+    { title: "我王佳乐", date: "2023-01-01", status: "进小区" },
+    { title: "我王佳乐", date: "2023-01-01", status: "进小区" },
+    { title: "我王佳乐", date: "2023-01-01", status: "进小区" },
+    { title: "我王佳乐", date: "2023-01-01", status: "进小区" },
+    { title: "我王佳乐", date: "2023-01-01", status: "进小区" },
+    { title: "我王佳乐", date: "2023-01-01", status: "进小区" },
+    { title: "我王佳乐", date: "2023-01-01", status: "进小区" },
+  ];
+
+  // 设备状态
+  device.value = [
+    { name: "摄像机", ok: "1000", bad: "0", red: "0" },
+    { name: "传感器", ok: "2000", bad: "0", red: "0" },
+    { name: "路由器", ok: "123", bad: "0", red: "0" },
+  ];
 };
 
-let option3 = {
-  title: {
-    text: "常住人口统计",
-    textStyle: {
-      color: "#fff",
-    },
-    show: false,
-  },
-  xAxis: {
-    type: "category",
-    boundaryGap: false,
-    data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-  },
-  yAxis: {
-    type: "value",
-  },
-  series: [
-    {
-      data: [820, 932, 901, 934, 1290, 1330, 1320],
-      type: "line",
-      areaStyle: {},
-    },
-  ],
-};
+// 控制根据地图数据进行改变图标信息
+const handleMessageFromChild = (message) => {
+  if (message === "历下区") {
+    option1.series[0].data = [
+      { value: 1048, name: "常住人口" },
+      { value: 735, name: "流出人口" },
+      { value: 484, name: "重点人员" },
+      { value: 300, name: "流入人口" },
+    ];
 
-//新闻资讯数据
-const newsdatas = reactive([]);
-const getList = async () => {
-  const list = await faceList();
-  newsdatas.value = [...list];
-  console.log("newsdatas", newsdatas.value);
-};
+    // 人口比例
+    option2.title.text = "各类人口占比1";
+    option2.series[0].data = [
+      { name: "流入人口", value: 3000 },
+      { name: "流出人口", value: 3000 },
+    ];
+    // 常住人口统计初始化
+    option3.series[0].data = [1330, 1330, 1330, 1330, 1330, 1330, 1320];
 
-const initWebsocket = () => {
-  var ws = new WebSocket("ws://127.0.0.1:8090"); //连接成功触发
-  ws.onopen = function () {
-    falert("连接成功");
-  };
-  //接收消息触发
-  ws.onmessage = function (data) {
-    console.log(data);
-  };
+    // 设备状态
+    device.value = [
+      { name: "摄像机", ok: "100", bad: "0", red: "0" },
+      { name: "传感器", ok: "200", bad: "0", red: "0" },
+      { name: "路由器", ok: "50", bad: "0", red: "0" },
+    ];
+
+    // 流动人口统计初始化
+    option5.series[0].data = [200, 200, 200, 200, 200, 200, 200];
+
+    //社区动态数据初始化
+    newsdatas.value = [
+      { title: "李小龙", date: "2023-01-01", status: "进小区" },
+      { title: "李小龙", date: "2023-01-01", status: "进小区" },
+      { title: "李小龙", date: "2023-01-01", status: "进小区" },
+      { title: "李小龙", date: "2023-01-01", status: "进小区" },
+      { title: "李小龙", date: "2023-01-01", status: "进小区" },
+      { title: "李小龙", date: "2023-01-01", status: "进小区" },
+      { title: "李小龙", date: "2023-01-01", status: "进小区" },
+      { title: "李小龙", date: "2023-01-01", status: "进小区" },
+      { title: "李小龙", date: "2023-01-01", status: "进小区" },
+      { title: "李小龙", date: "2023-01-01", status: "进小区" },
+      { title: "李小龙", date: "2023-01-01", status: "进小区" },
+      { title: "李小龙", date: "2023-01-01", status: "进小区" },
+    ];
+  } else if (message === "全国") {
+    initialization();
+  } else {
+    ElMessage.warning("没有数据");
+  }
 };
 
 onMounted(() => {
-  getList();
+  // getList();
 });
 </script>
 
@@ -220,7 +376,7 @@ onMounted(() => {
 }
 
 .indexscroll {
-  height: 20vh;
+  height: 25vh;
   width: 100%;
   overflow: hidden;
 }
@@ -239,15 +395,43 @@ onMounted(() => {
 }
 
 .indexscroll .item .date {
-  width: 4vw;
+  width: 10vw;
 }
 
 .indexscroll .item .status {
   width: 3vw;
-  color: #ff0157;
+  /* color: #ff0157; */
 }
 .titleStyle {
   text-align: center;
   color: antiquewhite;
+}
+
+.device {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  font-size: 20px;
+  width: 100%;
+  height: 90%;
+}
+
+.device-item {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  font-size: 1.3rem;
+}
+
+.device-item-ok {
+  color: green;
+}
+
+.device-item-bad {
+  color: yellow;
+}
+
+.device-item-red {
+  color: red;
 }
 </style>
--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,102 +1,277 @@
 <template>
+  <div @click="back" class="back">返回全国</div>
   <!-- 为 ECharts 准备一个定义了宽高的 DOM -->
-  <div ref="chart" style="width: 100%; height: 100%"></div>
+  <div ref="mapChart" id="mapChart" style="width: 100%; height: 100%"></div>
+  <!-- <BaiduMap
+    v-if="showMap"
+    :maplng="maplng"
+    :maplat="maplat"
+    @goback="showMap = $event"
+  ></BaiduMap> -->
 </template>
    
-   <script>
+<script setup>
 import * as echarts from "echarts";
-import chinaMap from "../assets/json/china.json";
+import { onMounted, defineEmits } from "vue";
+import { ref } from "vue";
+import { provinces } from "../utils/provincesmap";
+import { cityMap } from "../utils/citymap";
+// 导入组件
+// import BaiduMap from "../components/BaiduMap.vue";
+import axios from "axios";
 
-export default {
-  data() {
-    return {
-      items: [],
-    };
-  },
-  mounted() {
-    this.initChart();
-  },
-  methods: {
-    initChart() {
-      // 基于准备好的dom，初始化echarts实例
-      var myChart = echarts.init(this.$refs.chart);
-      echarts.registerMap("china", chinaMap);
+// 是否显示地图
+const showMap = ref(false);
+// 经度
+const maplng = ref(0);
+// 纬度
+const maplat = ref(0);
+//直辖市和特别行政区-只有二级地图，没有三级地图
+const special = ["北京", "天津", "上海", "重庆", "香港", "澳门"];
+// 地图数据
+let mapdata = [];
 
-      // 指定图表的配置项和数据
-      var option = {
-        geo: {
-          show: true,
-          type: "map",
-          map: "china",
-          // label: {
-          //   show: true, //文字标签显示
-          //   color: "#fff",
-          //   fontSize: 12,
-          // },
-          itemStyle: {
-            //图形样式
-            areaColor: "#3352c7", //背景色
-            borderColor: "#fff",
-            borderWidth: 1,
-          },
-          zoom: 1,
-          emphasis: {
-            //高亮状态
-            label: {
-              show: true, //文字标签显示
-              color: "#000",
-              fontSize: 14,
-            },
-            itemStyle: {
-              //图形样式
-              areaColor: "#fff", //背景色
-              borderColor: "#ccc",
-              borderWidth: 1,
-            },
-          },
-        },
-        series: [
-          {
-            type: "effectScatter", //带有涟漪特效动画的散点（气泡）图。利用动画特效可以将某些想要突出的数据进行视觉突出
-            coordinateSystem: "geo", //该系列使用的坐标系
-            rippleEffect: {
-              //涟漪特效相关配置。
-              period: 2,
-              brushType: "stroke",
-              scale: 5,
-            },
-            label: {
-              //标签
-              normal: {
-                show: true,
-                position: "right",
-              },
-            },
-            itemStyle: {
-              color: "#fff",
-            },
-            symbol: "circle", //标记的图形，还可以设置图片
-            symbolSize: function (val) {
-              return val[2];
-            },
-            data: [
-              {
-                // name: "贤文庄",
-                // value: [117.117744, 36.684818],
-                name: "济南",
-                value: [117, 36.65, 10],
-              },
-              {
-                name: "聊城",
-                value: [115.97, 36.45, 10],
-              },
-            ],
-          },
-        ],
-      };
-      // 使用刚指定的配置项和数据显示图表。
-      myChart.setOption(option);
+// 定义一个echarts对象
+var myChart = ref(null);
+
+const back = () => {
+  changedata('全国')
+  renderMap("china", mapdata);
+};
+
+// 控制全局数据变化
+const changdifang = ref("全国");
+const emit = defineEmits();
+
+// 初始化地图对象
+const mapChart = ref(null);
+
+//初始化绘制全国地图配置
+let option = {
+  backgroundColor: "#060E3F",
+  title: {
+    left: "center",
+    textStyle: {
+      color: "#fff",
+      fontSize: 16,
+      fontWeight: "normal",
+      fontFamily: "Microsoft YaHei",
+    },
+    subtextStyle: {
+      color: "#ccc",
+      fontSize: 13,
+      fontWeight: "normal",
+      fontFamily: "Microsoft YaHei",
     },
   },
+  tooltip: {
+    trigger: "item",
+    formatter: "{b}",
+  },
+  toolbox: {
+    show: true,
+    orient: "vertical",
+    left: "right",
+    top: "center",
+    feature: {
+      dataView: { readOnly: false },
+      restore: {},
+      saveAsImage: {},
+    },
+    iconStyle: {
+      normal: {
+        color: "#fff",
+      },
+    },
+  },
+  animationDuration: 1000,
+  animationEasing: "cubicOut",
+  animationDurationUpdate: 1000,
 };
+
+const markers = [
+  {
+    name: "济南",
+    value: [117, 36.65, 10],
+  },
+  {
+    name: "聊城",
+    value: [115.97, 36.45, 10],
+  },
+];
+
+//绘制地图的方法
+const renderMap = (map, data) => {
+  option.title.subtext = map;
+  option.series = [
+    {
+      name: map,
+      type: "map",
+      mapType: map,
+      roam: false,
+      nameMap: {
+        china: "中国",
+        show: false,
+      },
+      label: {
+        normal: {
+          show: true,
+          textStyle: {
+            color: "#999",
+            fontSize: 13,
+          },
+        },
+        emphasis: {
+          show: true,
+          textStyle: {
+            color: "#fff",
+            fontSize: 13,
+          },
+        },
+      },
+      itemStyle: {
+        normal: {
+          areaColor: "#3352c7", //背景色
+          borderColor: "color", // Change to the desired shade of blue
+        },
+        emphasis: {
+          areaColor: "darkorange",
+        },
+      },
+      data: data,
+    },
+    {
+      // Add a new series for markers
+      type: "scatter",
+      coordinateSystem: "geo",
+      data: markers.map((marker) => ({
+        name: marker.name,
+        value: marker.value,
+        symbol: "pin", // You can use other symbols for markers
+        symbolSize: 20, // Adjust the size of the marker symbol
+        label: {
+          show: true,
+          position: "right", // Adjust the position of the label
+          formatter: "{b}", // Label format
+        },
+      })),
+    },
+  ];
+  //渲染地图
+  myChart.value.setOption(option);
+};
+
+// 初始化地图
+const initChart = async () => {
+  // 基于准备好的dom，初始化echarts实例
+  myChart.value = echarts.init(document.getElementById("mapChart"));
+  let { data } = await axios.get("/map/china.json");
+  let d = [];
+  for (var i = 0; i < data.features.length; i++) {
+    d.push({
+      name: data.features[i].properties.name,
+    });
+  }
+  mapdata = d;
+  //注册地图
+  echarts.registerMap("china", data);
+  //绘制地图
+  renderMap("china", d);
+  // 使用刚指定的配置项和数据显示图表。
+  myChart.value.setOption(option);
+};
+
+const changedata = (name) => {
+  emit("sendMessageToParent", name);
+};
+
+onMounted(() => {
+  initChart();
+  myChart.value.on("click", async (params) => {
+    // 判断点击的是不是省份
+    if (params.name in provinces) {
+      // 如果点击的是34个省、市、自治区，绘制选中地区的二级地图
+      let { data } = await axios.get(
+        "/map/province/" + provinces[params.name] + ".json"
+      );
+      // 注册省份地图数据
+      echarts.registerMap(params.name, data);
+      // 添加省份对应的城市数据
+      var d = [];
+      for (var i = 0; i < data.features.length; i++) {
+        d.push({
+          name: data.features[i].properties.name,
+        });
+      }
+      // 重新绘制地图
+      renderMap(params.name, d);
+    } else if (params.seriesName in provinces) {
+      // 判断点击的是不是城市
+      // 如果是【直辖市/特别行政区】只有二级下钻
+      if (special.indexOf(params.seriesName) >= 0) {
+        // 打开百度地图
+        // let {
+        //   data: {
+        //     result: {
+        //       location: { lat, lng },
+        //     },
+        //   },
+        // } = await axios.get("/baidu/geocoding/v3/", {
+        //   params: {
+        //     address: params.seriesName + params.name,
+        //     ak: "CtDFZnPIhj7MjWLllGtFNyLpT4MpgGwv",
+        //     output: "json",
+        //   },
+        // });
+        maplng.value = lng;
+        maplat.value = lat;
+        showMap.value = true;
+      } else {
+        //显示县级地图
+        let { data } = await axios.get(
+          "/map/city/" + cityMap[params.name] + ".json"
+        );
+        // 注册城市地图数据
+        echarts.registerMap(params.name, data);
+        // 添加城市对应的区县数据
+        var d = [];
+        for (var i = 0; i < data.features.length; i++) {
+          d.push({
+            name: data.features[i].properties.name,
+          });
+        }
+        // 重新绘制地图
+        renderMap(params.name, d);
+      }
+    } else {
+      // 打开百度地图
+      // let {
+      //   data: {
+      //     result: {
+      //       location: { lat, lng },
+      //     },
+      //   },
+      // } = await axios.get("/baidu/geocoding/v3/", {
+      //   params: {
+      //     address: params.seriesName + params.name,
+      //     ak: "sICFGuAAPisdVnB5Bx6ILAADGPnRDusH",
+      //     output: "json",
+      //   },
+      // });
+      // maplng.value = lng;
+      // maplat.value = lat;
+      changedata(params.name);
+      showMap.value = true;
+    }
+  });
+});
 </script>
+<style>
+.back {
+  width: 80px;
+  height: 30px;
+  color: #fff;
+  margin: 30px;
+  font-size: 18px;
+}
+</style>
